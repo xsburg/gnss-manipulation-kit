3c90a189a2b503bbd58fbeef3a6a356f9bbcd113
sburg <sburg@ifz.ru>
2012-04-04 18:36:20 +0000

    TODO: raw messages in MySqlSource, correct messages order in epochs.

--- DatabaseToJps/main.cpp
+++ DatabaseToJps/main.cpp
@@ -1,19 +1,88 @@
 #include <QtCore/QtCore>
 #include <clocale>
 #include <locale>
+#include <iostream>
 #include "ProjectBase/Logger.h"
 #include "ProjectBase/Path.h"
 #include "ProjectBase/Connection.h"
 #include "JpsFile.h"
 #include "MySqlSink.h"
+#include "MySqlSource.h"
 
 using namespace ProjectBase;
 using namespace Greis;
 
+void parseArguments(const QCoreApplication& a, QString& outFilename, QDateTime& from, QDateTime& to)
+{
+    auto args = a.arguments();
+    auto usageStr = QString("Usage: 'app.exe <output-filename>' [<date-from> [date-to]]\r\n") + 
+        QString("\r\n") + 
+        QString("Where:\r\n") + 
+        QString("    <output-file> - path to output jps file.\r\n") + 
+        QString("    <date-from>, <date-to> - date range should be specified in either 'yyyy-MM-dd HH:mm:ss' or `msecs since epoch` format. \r\n") + 
+        QString("        Date will be set to [NOW - 1 day, NOW] if omitted. \r\n") + 
+        QString("        All dates should be specified in UTC.\r\n") + 
+        QString("\r\n");
+
+    if (args.count() < 2)
+    {
+        std::cout << usageStr.toAscii().data() << std::endl;
+        throw Exception();
+    }
+
+    outFilename = args[1];
+
+    if (args.count() == 2)
+    {
+        from = QDateTime::currentDateTimeUtc().addDays(-1);
+        to = QDateTime::currentDateTimeUtc();
+    } else if (args.count() == 3)
+    {
+        bool bOk;
+        unsigned long long val = args[2].toULongLong(&bOk);
+        if (bOk)
+        {
+            from = QDateTime::fromMSecsSinceEpoch(val);
+        } else {
+            from = QDateTime::fromString(args[2], "yyyy-MM-dd HH:mm:ss");
+            from.setTimeSpec(Qt::UTC);
+        }
+        if (!from.isValid())
+        {
+            throw Exception("Invalid `from` argument.");
+        }
+        to = QDateTime::currentDateTimeUtc();
+    } else {
+        bool bOk;
+        unsigned long long val = args[2].toULongLong(&bOk);
+        if (bOk)
+        {
+            from = QDateTime::fromMSecsSinceEpoch(val);
+        } else {
+            from = QDateTime::fromString(args[2], "yyyy-MM-dd HH:mm:ss");
+            from.setTimeSpec(Qt::UTC);
+        }
+        if (!from.isValid())
+        {
+            throw Exception("Invalid `from` argument.");
+        }
+        val = args[3].toULongLong(&bOk);
+        if (bOk)
+        {
+            to = QDateTime::fromMSecsSinceEpoch(val);
+        } else {
+            to = QDateTime::fromString(args[3], "yyyy-MM-dd HH:mm:ss");
+            to.setTimeSpec(Qt::UTC);
+        }
+        if (!to.isValid())
+        {
+            throw Exception("Invalid `to` argument.");
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
-    bool wrapIntoTransaction;
-    Connection::SharedPtr_t connection;
     try
     {
         std::setlocale(LC_ALL, "Russian_Russia.1251");
@@ -30,47 +99,40 @@ int main(int argc, char **argv)
         sLogger.Initialize(Path::Combine(Path::ApplicationDirPath(), "logger.config.xml"));
         sIniSettings.Initialize(Path::Combine(Path::ApplicationDirPath(), "config.ini"));
 
-        // Connecting to database
-        wrapIntoTransaction = sIniSettings.value("WrapIntoTransaction", false).toBool();
-        int inserterBatchSize = sIniSettings.value("inserterBatchSize", 10000).toInt();
-        connection = Connection::FromSettings("Db");
+        QString outFilename;
+        QDateTime from;
+        QDateTime to;
+        parseArguments(a, outFilename, from, to);
+
+        auto connection = Connection::FromSettings("Db");
+        sLogger.Info(QString("Connecting to database `%1`...").arg(connection->DatabaseName));
         connection->Connect();
 
-        auto args = a.arguments();
-        args.pop_front();
+        auto mySqlSource = make_unique<MySqlSource>(connection.get());
 
-        foreach (QString filename, args)
-        {
-            sLogger.Info(QString("Reading file `%1`...").arg(filename));
-            auto file = JpsFile::FromFile(filename);
+        sLogger.Info("Done!");
+        sLogger.Info(QString("Reading data for range [`%1`, `%2`] (%3 - %4)...")
+            .arg(from.toString("dd.MM.yyyy HH:mm:ss"))
+            .arg(to.toString("dd.MM.yyyy HH:mm:ss"))
+            .arg(from.toMSecsSinceEpoch())
+            .arg(to.toMSecsSinceEpoch()));
+        auto jpsFile = mySqlSource->ReadRange(from, to);
 
-            sLogger.Info(QString("Writing data into database `%1`...").arg(connection->DatabaseName));
-            if (wrapIntoTransaction)
-            {
-                sLogger.Info("Starting a new transaction...");
-                connection->Database().transaction();
-            }
-            {
-                auto sink = make_unique<MySqlSink>(connection.get(), inserterBatchSize);
-                sink->AddJpsFile(file.get());
-                sink->Flush();
-            }
-            if (wrapIntoTransaction)
-            {
-                connection->Database().commit();
-                sLogger.Info("Transaction has been committed.");
-            }
-            sLogger.Info(QString("Data from `%1` has been successfully imported into database.").arg(filename));
-        }
+        sLogger.Info("Done!");
+        sLogger.Info("Binarizing data...");
+        auto ba = jpsFile->ToByteArray();
+
+        sLogger.Info("Done!");
+        sLogger.Info("Saving data...");
+        auto out = File::CreateBinary(outFilename);
+        out->write(ba);
+        out->close();
+        sLogger.Info(QString("Done!\r\nData successfully saved in `%1`.").arg(outFilename));
+
         return 0;
     }
     catch (Exception& e)
     {
-        if (wrapIntoTransaction)
-        {
-            connection->Database().rollback();
-            sLogger.Info("The transaction has been rolled back.");
-        }
         sLogger.Error(e.what());
         return 1;
     }
